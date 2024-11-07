4a0667dc1d1594580b4087abeaf3ee54f0aa9db6
cooleo273 <leulteferi273@gmail.com>
2024-11-07 11:32:15 +0300

    making the images on the background

--- src/components/imageslider/ImageSlider.tsx
+++ src/components/imageslider/ImageSlider.tsx
@@ -1,48 +1,70 @@
-import React from 'react';
-import Slider from 'react-slick';
-import img1 from '../../assets/4th-01.jpg';
-import img3 from '../../assets/mt-1869-gallery-09bg.jpg'; 
-import img2 from '../../assets/image-25.png'; 
-import "./index.css"
+import React from "react";
+import Slider from "react-slick";
+import img1 from "../../assets/4th-01.jpg";
+import img3 from "../../assets/mt-1869-gallery-09bg.jpg";
+import img2 from "../../assets/image-25.png";
+import "./index.css";
 
 // Left Arrow Component
-
 
 // Right Arrow Component
 
 const ImageSlider: React.FC = () => {
-    const settings = {
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        arrows: true,
-        pauseOnHover: false,
-        
-    };
+  const settings = {
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    pauseOnHover: false,
+  };
 
-    return (
-        <Slider {...settings}>
-            <div>
-                <img src={img1} alt="Image 1" className="slider-image" style={sliderImageStyle} />
-            </div>
-            <div>
-                <img src={img2} alt="Image 2" className="slider-image" style={sliderImageStyle} />
-            </div>
-            <div>
-                <img src={img3} alt="Image 3" className="slider-image" style={sliderImageStyle} />
-            </div>
-        </Slider>
-    );
+  return (
+    <Slider {...settings}>
+      <div>
+        <div
+          style={{
+            ...sliderImageStyle,
+            backgroundImage: `url("${img1}")`,
+            backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          }}
+        ></div>
+      </div>
+      <div>
+        <div
+          style={{
+            ...sliderImageStyle,
+            backgroundImage: `url("${img2}")`,
+            backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          }}
+        ></div>
+      </div>
+      <div>
+        <div
+          style={{
+            ...sliderImageStyle,
+            backgroundImage: `url("${img2}")`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+        ></div>
+      </div>
+    </Slider>
+  );
 };
 
 // Custom styles for the images
 const sliderImageStyle: React.CSSProperties = {
-    width: '100%',
-    height: '45rem',
-    objectFit: "cover"
+  width: "100%",
+  height: "45rem",
+  objectFit: "cover",
 };
 
 export default ImageSlider;
