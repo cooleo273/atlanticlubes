5aff173c861fd67b4c98c07353ad2aa32e411ead
cooleo273 <leulteferi273@gmail.com>
2024-11-15 19:37:59 +0300

    style some stuff

---
+++ src/pages/Lubricants/types.ts
@@ -0,0 +1,11 @@
+export interface InventoryItem {
+    id: number;
+    inventory_name: string;
+    image: string;
+    description: string;
+}
+
+export interface Category {
+    id: number;
+    name: string;
+}
