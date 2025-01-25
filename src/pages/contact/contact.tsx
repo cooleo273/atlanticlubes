d260ebd7effd4a5599dc454fc42d829b158812b8
cooleo273 <leulteferi273@gmail.com>
2025-01-25 13:26:54 +0300

    styling and the loading in the contact page

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
-
-import emailjs from '@emailjs/browser'; // Import emailjs library
-
+import emailjs from "@emailjs/browser"; // Import emailjs library
 import { Cobe } from "../../components/ui/globe";
-
 
 const Contact: React.FC = () => {
   const [formData, setFormData] = useState({
@@ -14,6 +11,10 @@ const Contact: React.FC = () => {
     message: "",
     notARobot: false,
   });
+
+  const [loading, setLoading] = useState(false); // State to manage loading
+  const [success, setSuccess] = useState(false); // State to manage success message
+  const [error, setError] = useState<string | null>(null); // State to manage error message
 
   // Handle form input changes
   const handleInputChange = (
@@ -39,6 +40,10 @@ const Contact: React.FC = () => {
     e.preventDefault();
 
     if (formData.notARobot) {
+      setLoading(true); // Set loading to true when form is submitted
+      setSuccess(false); // Reset success message
+      setError(null); // Reset error message
+
       const templateParams = {
         companyName: formData.companyName,
         name: formData.name,
@@ -50,19 +55,29 @@ const Contact: React.FC = () => {
       // Send email using EmailJS with send method (not sendForm)
       emailjs
         .send(
-          "service_hq3atgl",        // Service ID
-          "template_rnybs0b",        // Template ID
-          templateParams,            // Parameters (mapped to your email template)
-          "XKdjkHjDhXBnjODka"       // Your EmailJS user ID
+          "service_hq3atgl", // Service ID
+          "template_rnybs0b", // Template ID
+          templateParams, // Parameters (mapped to your email template)
+          "XKdjkHjDhXBnjODka" // Your EmailJS user ID
         )
         .then(
           (response) => {
             console.log("Email sent successfully:", response);
-            alert("Your message has been sent successfully!");
+            setSuccess(true); // Set success to true when email is sent
+            setFormData({
+              companyName: "",
+              name: "",
+              phone: "",
+              email: "",
+              message: "",
+              notARobot: false,
+            }); // Clear form after successful submission
+            setLoading(false); // Set loading to false when email is sent
           },
           (error) => {
             console.log("Failed to send email:", error);
-            alert("There was an issue sending your message. Please try again.");
+            setError("There was an issue sending your message. Please try again.");
+            setLoading(false); // Set loading to false on error
           }
         );
     } else {
@@ -71,119 +86,147 @@ const Contact: React.FC = () => {
   };
 
   return (
-    <div className="contact-us text-gray-800">
-
-
-      <div className="flex flex-col lg:flex-row p-30 px-8 lg:px-10 py-4">
-        {/* Contact Form */}
-        <div className="flex-1 bg-white p-8 rounded-lg shadow-lg ">
-          <form onSubmit={handleSubmit} className="space-y-5">
-            {/* Company Name */}
-            <div className="relative">
-              
-              <input
-                type="text"
-                id="companyName"
-                placeholder="Company Name"
-                name="companyName"
-                value={formData.companyName}
-                onChange={handleInputChange}
-                required
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-              />
+    <div className="contact-us bg-gray-200 min-h-screen flex justify-center items-center px-4 py-12">
+      <div className="w-full flex space-x-12">
+        {/* Form Section */}
+        <div className="flex-1">
+          <div className="bg-white p-6 rounded-lg shadow-2xl space-y-6">
+            <div className="text-center">
+              <h2 className="text-2xl font-bold text-[#F57F27]">Get in Touch</h2>
+              <p className="mt-2 text-md text-gray-600">
+                We're here to help! Please fill out the form below and we'll
+                get back to you as soon as possible.
+              </p>
             </div>
 
-            {/* Name */}
-            <div className="relative">
-             
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Name"
-                value={formData.name}
-                onChange={handleInputChange}
-                required
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-              />
-            </div>
+            {/* Success and Error Messages */}
+            {success && (
+              <div className="text-center text-green-600 font-semibold">
+                Your message has been sent successfully!
+              </div>
+            )}
+            {error && (
+              <div className="text-center text-red-600 font-semibold">
+                {error}
+              </div>
+            )}
 
-            {/* Phone */}
-            <div className="relative">
-              
-              <input
-                type="tel"
-                id="phone"
-                placeholder="Phone Number"
-                name="phone"
-                value={formData.phone}
-                onChange={handleInputChange}
-                required
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-              />
-            </div>
+            <form onSubmit={handleSubmit} className="space-y-5">
+              {/* Company Name */}
+              <div>
+                <input
+                  type="text"
+                  id="companyName"
+                  placeholder="Company Name"
+                  name="companyName"
+                  value={formData.companyName}
+                  onChange={handleInputChange}
+                  style={{fontSize: "0.9rem"}}
+                  
+                  required
+                  className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#F57F27] transition-shadow"
+                />
+              </div>
 
-            {/* Email */}
-            <div className="relative">
-              
-              <input
-                type="email"
-                placeholder="Email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleInputChange}
-                required
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-              />
-            </div>
+              {/* Name */}
+              <div>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Your Name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  style={{fontSize: "0.9rem"}}
+                  required
+                  className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#F57F27] transition-shadow"
+                />
+              </div>
 
-            {/* Message */}
-            <div className="relative">
-              
-              <textarea
-                id="message"
-                placeholder="Enter your message"
-                name="message"
-                value={formData.message}
-                onChange={handleInputChange}
-                required
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-                rows={1}
-              />
-            </div>
+              {/* Phone */}
+              <div>
+                <input
+                  type="tel"
+                  id="phone"
+                  placeholder="Phone Number"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleInputChange}
+                  style={{fontSize: "0.9rem"}}
+                  required
+                  className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#F57F27] transition-shadow"
+                />
+              </div>
 
-            {/* CAPTCHA */}
-            <div className="flex items-center space-x-2">
-              <input
-                type="checkbox"
-                name="notARobot"
-                checked={formData.notARobot}
-                onChange={handleInputChange}
-                required
-                className="h-5 w-5 text-blue-500 focus:ring-blue-500"
-              />
-              <label className="font-semibold">I am not a robot</label>
-            </div>
+              {/* Email */}
+              <div>
+                <input
+                  type="email"
+                  placeholder="Email Address"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  style={{fontSize: "0.9rem"}}
+                  required
+                  className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#F57F27] transition-shadow"
+                />
+              </div>
 
-            {/* Submit Button */}
-            <button
-              type="submit"
-              className="w-full bg-black hover:bg-black text-white font-semibold py-3 rounded-lg transition"
-            >
-              Send Message
-            </button>
-          </form>
-          
+              {/* Message */}
+              <div>
+                <textarea
+                  id="message"
+                  placeholder="Your Message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleInputChange}
+                  required
+                  className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#F57F27] transition-shadow"
+                  style={{fontSize: "0.9rem"}}
+                  rows={2}
+                />
+              </div>
+
+              {/* CAPTCHA */}
+              <div className="flex items-center space-x-3">
+                <input
+                  type="checkbox"
+                  name="notARobot"
+                  checked={formData.notARobot}
+                  onChange={handleInputChange}
+                  required
+                  className="h-5 w-5 text-[#EF353B] focus:ring-[#EF353B]"
+                />
+                <label className="text-sm text-gray-600">
+                  I am not a robot
+                </label>
+              </div>
+
+              {/* Submit Button */}
+              <div>
+                <button
+                  type="submit"
+                  disabled={loading} // Disable button if loading
+                  className={`w-full p-3 text-white bg-[#F57F27] rounded-lg font-semibold transition-all hover:bg-[#EF353B] ${
+                    loading ? "opacity-50 cursor-not-allowed" : ""
+                  }`}
+                >
+                  {loading ? (
+                    <div className="animate-spin h-5 w-5 border-4 border-t-4 border-white rounded-full"></div>
+                  ) : (
+                    "Send Message"
+                  )}
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
 
-       <div className="flex justify-center items-start flex-1">
-        <Cobe/>
-       </div>
-        
-
-
-       
+        {/* Cobe Section */}
+        <div className="flex-1 max-w-lg">
+          <Cobe />
+        </div>
       </div>
     </div>
   );
